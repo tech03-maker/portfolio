d169ee789e6eb802d14a3bec4d536899032b080a
Jumah Muhamad <jumahtech@gmail.com>
2023-12-17 14:32:43 +0100

    prettier formatter edit

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import "./Hero.css";
 import "animate.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWhatsappSquare } from "@fortawesome/free-brands-svg-icons";
-import hero_pic from "../../assets/IMG-20231216-WA0063.jpg"
+import hero_pic from "../../assets/IMG-20231216-WA0063.jpg";
 
 const Hero = () => {
   return (
@@ -25,19 +25,25 @@ const Hero = () => {
               </div>
             </a>
             <h4 className="mt-3 fw-bold">Skills:</h4>
-                <div className="html text-center p-1 mt-3">
-                    <p className="pt-3 fw-bold text-white">HTML | 100%</p>
-                </div>
-                <div className="css text-center p-1">
-                    <p className="pt-3 fw-bold text-white">CSS | 100%</p>
-                </div>
-                <div className="html bg-primary text-center p-1 mb-3">
-                    <p className="pt-3 fw-bold text-white">BOOTSTRAP | 100%</p>
-                </div>
-                <a className="skills_link text-dark" href="#">View Skills</a>
+            <div className="html text-center p-1 mt-3">
+              <p className="pt-3 fw-bold text-white">HTML | 100%</p>
+            </div>
+            <div className="css text-center p-1">
+              <p className="pt-3 fw-bold text-white">CSS | 100%</p>
+            </div>
+            <div className="html bg-primary text-center p-1 mb-3">
+              <p className="pt-3 fw-bold text-white">BOOTSTRAP | 100%</p>
+            </div>
+            <a className="skills_link text-dark" href="#">
+              View Skills
+            </a>
           </div>
           <div className="col-12 col-sm-12 col-md-6 col-lg-6 mb-5">
-          <img className="image w-100 animate__animated animate__fadeInDown" src={hero_pic} alt="" />
+            <img
+              className="image w-100 animate__animated animate__fadeInDown"
+              src={hero_pic}
+              alt=""
+            />
           </div>
         </div>
       </div>
